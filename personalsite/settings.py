6dbc1e7930b57af7f6d058ff0808dac855a928b7
emre.atadil <emre.atadil@yandex.com>
2019-08-05 20:28:51 +0300

    Emre Atadil Commit

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -140,7 +140,3 @@ STATICFILES_DIRS = [STATIC_DIR]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/img/'
 # LOGIN_URL = '/giris/'
-
-import dj_database_url
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
