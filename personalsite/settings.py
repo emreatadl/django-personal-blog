28d9eca6b4b80f9093b1cac74791f8d7f4289238
emre.atadil <emre.atadil@yandex.com>
2019-08-06 13:36:03 +0300

    edit settings

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -30,9 +30,9 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 SECRET_KEY = '9sx4unctsv^4eoc6grakexmti=i=7$dk5#ctyg**6i%n=b)t)6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['emreatadil.com', 'www.emreatadil.com', ]
 
 
 # Application definition
