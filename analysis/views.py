b024289dc120e09b8f936385e83eb7622350f1c6
emre.atadil <emre.atadil@yandex.com>
2019-11-15 13:35:18 +0300

    Delete analysis files

--- analysis/views.py
+++
@@ -1,6 +0,0 @@
-from django.db.models import Count, Q
-from django.shortcuts import render
-
-
-def analysis_index(request):
-    return render(request, 'charts.html')
--- personalsite/urls.py
+++ personalsite/urls.py
@@ -5,10 +5,7 @@ from django.urls import *
 from django.urls import path, include
 from blog import views
 
-
 urlpatterns = [
-    path('ea/kp/admin/', include('material.admin.urls')),
-    url(r'', include('blog.urls')),
-    path(r'analiz/', include('analysis.urls')),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('ea/kp/admin/', include('material.admin.urls')),
+                  url(r'', include('blog.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
