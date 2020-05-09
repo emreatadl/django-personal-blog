df75ab9bb5e0a824ce561fe529a8e6911b5e8d83
Emre Atadil <emreatadl@gmail.com>
2020-05-09 19:45:47 +0300

    add popups

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = '9sx4unctsv^4eoc6grakexmti=i=7$dk5#ctyg**6i%n=b)t)6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", ]
 # Application definition
 
 INSTALLED_APPS = [
